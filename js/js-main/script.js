db188a071721b83a8530497aa59bf98a330d5336
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-04-19 17:23:28 +0200

    JS FORMULAIRE
    OPTION JS FORMULAIRE

--- js/js-main/script.js
+++ js/js-main/script.js
@@ -1,5 +1,23 @@
+ document.forms["inscription"].addEventListener("submit", function(e) {
+   var erreur;
+   var inputs = this.getElementsByTagName("input");
+
+   if(erreur) {
+     e.preventDefault();
+     document.getElementById("erreur").innerHTML = erreur;
+     return false;
+   } else {
+     alert("Formulaire envoyé");
+   }
+
+   var nom = document.getElementById("nom");
+    var prenom = document.getElementById("prenom");
+    var telephone = document.getElementById("telephone");
+    var email = document.getElementById("email");
 
 
-document.querySelector('.bouton-theme').addEventListener('click', function() {
-  document.querySelector('footer').classList.toggle('theme-nuit');
-});
+      if (!email.value) {
+        erreur = "Veuillez renseigner un email";
+      }
+
+ });
