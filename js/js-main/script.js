c91688c251831da1aaf27ce9759304d693c922c4
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-04-24 13:36:49 +0200

    js

--- js/js-main/script.js
+++ js/js-main/script.js
@@ -1,32 +1,32 @@
 
 //JS PAGE FORMULAIRE //
 
- 
- document.forms["inscription"].addEventListener("submit", function(e) {
-   var erreur;
-   var inputs = this.getElementsByTagName("input");
-
-   if(erreur) {
-     e.preventDefault();
-     document.getElementById("erreur").innerHTML = erreur;
-     return false;
-   } else {
-     alert("Formulaire envoyé");
-   }
-
-   var nom = document.getElementById("nom");
-    var prenom = document.getElementById("prenom");
-    var telephone = document.getElementById("telephone");
-    var email = document.getElementById("email");
-
-
-      if (!email.value) {
-        erreur = "Veuillez renseigner un email";
-      }
-
- });
-
-
+ // 
+ // document.forms["inscription"].addEventListener("submit", function(e) {
+ //   var erreur;
+ //   var inputs = this.getElementsByTagName("input");
+ //
+ //   if(erreur) {
+ //     e.preventDefault();
+ //     document.getElementById("erreur").innerHTML = erreur;
+ //     return false;
+ //   } else {
+ //     alert("Formulaire envoyé");
+ //   }
+ //
+ //   var nom = document.getElementById("nom");
+ //    var prenom = document.getElementById("prenom");
+ //    var telephone = document.getElementById("telephone");
+ //    var email = document.getElementById("email");
+ //
+ //
+ //      if (!email.value) {
+ //        erreur = "Veuillez renseigner un email";
+ //      }
+ //
+ // });
+ //
+ //
 
 
 // JS PAGE INDEX //
