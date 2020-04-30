99fcdfa4ffc94ccd02310c0c04a85af4aa6fc53e
soraya-berguiga <soraya.berguiga05@gmail.com>
2020-04-30 13:16:09 +0200

    paje js fomulaire
    paje js fomulaire

--- js/js-main/script.js
+++ js/js-main/script.js
@@ -1,33 +1,4 @@
 
-//JS PAGE FORMULAIRE //
-
- // 
- // document.forms["inscription"].addEventListener("submit", function(e) {
- //   var erreur;
- //   var inputs = this.getElementsByTagName("input");
- //
- //   if(erreur) {
- //     e.preventDefault();
- //     document.getElementById("erreur").innerHTML = erreur;
- //     return false;
- //   } else {
- //     alert("Formulaire envoyé");
- //   }
- //
- //   var nom = document.getElementById("nom");
- //    var prenom = document.getElementById("prenom");
- //    var telephone = document.getElementById("telephone");
- //    var email = document.getElementById("email");
- //
- //
- //      if (!email.value) {
- //        erreur = "Veuillez renseigner un email";
- //      }
- //
- // });
- //
- //
-
 
 // JS PAGE INDEX //
 
@@ -35,6 +6,3 @@
 document.querySelector('.bouton-theme').addEventListener('click', function() {
 document.querySelector('footer').classList.add('theme-nuit');
 });
-
-
-// toggle
